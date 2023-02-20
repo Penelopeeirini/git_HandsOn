fa165208dcedcbe27830cfeef52c51f84b3d302a
Your Name <you@example.com>
2023-02-20 20:11:42 +0100

    nucleotide percentages

--- seqClass.py
+++ seqClass.py
@@ -2,10 +2,13 @@
 
 import sys, re
 from argparse import ArgumentParser
+from collections import Counter
+
 
 parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
 parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
 parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
+#parser.add_argument("-n", "--nucleotide", action= store_true, help = "Print nucleotide percentage") 
 if len(sys.argv) == 1:
     parser.print_help()
     sys.exit(1)
@@ -26,7 +29,22 @@ if args.motif:
     args.motif = args.motif.upper()
     print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
     if re.search(args.motif, args.seq):
-        print("FOUND motif2")
+        print("FOUND motif")
     else:
         print("NOT FOUND")
 #now we are  able to classify rna and dna correctly
+
+# Count the occurrences of each nucleotide in the sequence
+counts = Counter(args.seq)
+
+# Compute the total number of nucleotides in the sequence
+total_nucleotides = sum(counts.values())
+
+# Compute the percentage of each nucleotide
+percentages = {nucleotide: count / total_nucleotides * 100 for nucleotide, count in counts.items()}
+
+# Print the percentages
+print("Nucleotide percentages:")
+for nucleotide, percentage in percentages.items():
+  print(f"{nucleotide}: {percentage:.2f}%")
+
